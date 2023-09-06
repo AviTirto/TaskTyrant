f3a61bf9a41d715ed20386d23272a7f760f49a17
AviTirto <avi.tirto@gmail.com>
2023-09-06 16:22:23 -0500

    added color to android ripple

--- TaskTyrant/components/CircleButton/CircleButton.js
+++ TaskTyrant/components/CircleButton/CircleButton.js
@@ -4,7 +4,7 @@ import styles from './CircleButtonStyles'
 const CircleButton = (props) => {
     return (
         <View style={styles.innerContainer}>
-            <Pressable style={styles.outerContainer} onPress={props.onPress} android_ripple={}>
+            <Pressable style={styles.outerContainer} onPress={props.onPress} android_ripple={{color: 'white'}}>
                 <Text style={styles.text}>{props.children}</Text>
             </Pressable>
         </View>
