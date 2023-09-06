be4ff21414bccbebb1ae27bfb0359dbc05d53b01
AviTirto <avi.tirto@gmail.com>
2023-09-06 16:19:38 -0500

    circle button styles

--- TaskTyrant/components/CircleButton/CircleButton.js
+++ TaskTyrant/components/CircleButton/CircleButton.js
@@ -4,7 +4,7 @@ import styles from './CircleButtonStyles'
 const CircleButton = (props) => {
     return (
         <View style={styles.innerContainer}>
-            <Pressable style={styles.outerContainer} onPress={props.onPress}>
+            <Pressable style={styles.outerContainer} onPress={props.onPress} android_ripple={}>
                 <Text style={styles.text}>{props.children}</Text>
             </Pressable>
         </View>
