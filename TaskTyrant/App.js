031976cadc82f71ca55ecd90e4ade3db995e63d9
AviTirto <avi.tirto@gmail.com>
2023-09-06 16:10:06 -0500

    implementing the UI Kitten Library

--- TaskTyrant/App.js
+++ TaskTyrant/App.js
@@ -1,9 +1,21 @@
 import TaskList from "./components/TaskList/TaskList";
+import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import * as eva from '@eva-design/eva';
 
 
-export default function App() {
+function App() {
   return (
-    <TaskList/>
+    <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text category='h1'>HOME</Text>
+      <TaskList/>
+    </Layout>
   )
 }
+
+
+export default () => (
+  <ApplicationProvider {...eva} theme={eva.light}>
+    <App />
+  </ApplicationProvider>
+);
 
